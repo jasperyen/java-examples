4ea90da581f843e5d0eac2f07ed0e61bffc71e03
ChiaChen_Yen <jasper1996826@gmail.com>
2018-01-10 23:25:34 +0800

    Create AWSObjectDetection !

--- AWSObjectDetection/src/awsobjectdetection/AWSObjectDetection.java
+++ AWSObjectDetection/src/awsobjectdetection/AWSObjectDetection.java
@@ -1,9 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package awsobjectdetection;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.List;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
+import com.amazonaws.services.rekognition.model.DetectLabelsResult;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.Label;
+import com.amazonaws.util.IOUtils;
 
 /**
  *
@@ -11,11 +22,42 @@ package awsobjectdetection;
  */
 public class AWSObjectDetection {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    static private final AWSCredentials credentials;
+    static private final AmazonRekognition rekognitionClient;
+    
+    static {
+        
+        credentials = new ProfileCredentialsProvider("default").getCredentials();
+        
+        rekognitionClient = AmazonRekognitionClientBuilder
+                                .standard()
+                                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                                .build();
+    }
+    
+    public static void main(String[] args) throws Exception {
+    	String photo="Koala.jpg";
+        
+        ByteBuffer imageBytes;
+        try (InputStream inputStream = new FileInputStream(new File(photo))) {
+            imageBytes = ByteBuffer.wrap(IOUtils.toByteArray(inputStream));
+        }
+        
+        Image image = new Image(). withBytes(imageBytes);
+        
+        DetectLabelsRequest request = new DetectLabelsRequest().withImage(image);
+
+        System.out.println("Start detect . . . " + photo);
+        
+        DetectLabelsResult result = rekognitionClient.detectLabels(request);
+        List<Label> labels = result.getLabels();
+
+        System.out.println("Detected labels for " + photo);
+        
+        labels.forEach( label -> {
+            System.out.println(label.getName() + ": " + label.getConfidence());
+        });
     }
     
 }
